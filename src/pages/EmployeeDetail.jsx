e69316dc547ff077360a52b1ad9b0a6b469497f6
muzaffarhaque <muzaffarhaque777@gmail.com>
2026-07-06 00:17:33 +0530

    details page start

--- src/pages/EmployeeDetail.jsx
+++ src/pages/EmployeeDetail.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
 export default function EmployeeDetail() {
-  return (
-    <div>
-       <h4 className="fs-18-14">Good Morning, Muzaffarul haque!</h4>
+    return (
+        <div>
+            <h4 className="fs-18-14">Good Morning, Muzaffarul haque!</h4>
+            <div className="details-main-wrapper">
+                <div className="left-detail">
+                  <div className="details-profile-frame">
+                    
+                  </div>
+                </div>
+                <div className="right-details-wrapper"></div>
+            </div>
 
-    </div>
-  )
+        </div>
+    )
 }
